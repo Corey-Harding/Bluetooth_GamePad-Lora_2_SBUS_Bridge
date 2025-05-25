422116653ba8135a1fa6382d7285173b2b8ff2c0
Corey-Harding <91717711+Corey-Harding@users.noreply.github.com>
2025-05-25 05:21:58 +0000

    Upload RX Files Via WebUI

---
+++ ConvertLoraPacketToSBUS.h
@@ -0,0 +1,699 @@
+//WARNING!
+//The below code is a proof of concept only!
+//
+//DO NOT flash this code onto a device which is operated in a region with duty cycle restrictions/dwell time restrictions/air time restrictions
+//
+//The below code is intended to be ported to either FHSS with the ability to become more compliant with air time related restrictions
+//or ported to a device which operates on a frequency that does not have the same type of restrictions. 2.4ghz Lora?
+//
+//As it currently stands this code will not comply with duty cycle restrictions/dwell time restrictions/air time restrictions and needs further work before actual use
+//The author(s) take no responsibility for your use or misuse of this code.
+//It is up to the end user to research, know, and comply with all of the laws and regulations in the region in which they are operating in.
+//The end user, and only the end user, is responsible for the end user's actions.
+//
+//Again this is a proof of concept example only and not meant for actual use.  You have been warned!
+//
+//XBOX BT to SBUS related Code is forked from: https://github.com/mechzrobotics/Unitree_GO2_SBUS
+//Encoding/Decoding of LORA Packets containing XBOX Axis/Button States by Corey Harding
+
+/* SBUS object, writing SBUS */
+//SBUS Serial, RX Pin, TX Pin
+//Values for Xiao Esp32s3, Pin 43(D6) is TX
+bfs::SbusTx sbus_tx(&SERIAL_INTERFACE, SERIAL_RX_PIN, SERIAL_TX_PIN, true);
+/* SBUS data */
+bfs::SbusData data;
+
+unsigned long StartTime = 0;
+unsigned long CurrentTime = 0;
+unsigned long ElapsedTime = 0;
+
+struct SBUS_DATA{
+  int32_t CH1;
+  int32_t CH2;
+  int32_t CH3;
+  int32_t CH4;
+  int32_t CH5;
+  int32_t CH6;
+  int32_t CH7;
+  int32_t CH8;
+};
+
+// Button State Flags:
+bool A_Flag = 0;
+bool B_Flag = 0;
+bool X_Flag = 0;
+bool Y_Flag = 0;
+bool L1_Flag = 0;
+bool L2_Flag = 0;
+bool R1_Flag = 0;
+bool R2_Flag = 0;
+bool D_UP_Flag = 0;
+bool D_DOWN_Flag = 0;
+bool D_LEFT_Flag = 0;
+bool D_RIGHT_Flag = 0;
+bool START_Flag = 0;
+bool SELECT_Flag = 0;
+bool XBOX_Flag = 0;
+bool SHARE_Flag = 0;
+
+// DOG State Flags: //Unitree Go2 Specific
+bool HIGH_STAND_Flag = 1;
+bool OBSTACLE_Flag = 0;
+int8_t WALK_FLAG = 0;
+
+void writeSBUS(SBUS_DATA my_data){
+  data.ch[0] = my_data.CH1;
+  data.ch[1] = my_data.CH2;
+  data.ch[2] = my_data.CH3;
+  data.ch[3] = my_data.CH4;
+  data.ch[4] = my_data.CH5; // CH5 default 992
+  data.ch[5] = my_data.CH6; // CH6
+  data.ch[6] = my_data.CH7; // CH7
+  data.ch[7] = my_data.CH8; // CH8
+  data.ch[8] = 1690; //CH9 static PWM value as shown in Unitree Go2 SBUS Documentation
+  data.ch[9] = 154; //CH10 static PWM value as shown in Unitree Go2 SBUS Documentation
+  //Output SBUS Data
+  sbus_tx.data(data);
+  sbus_tx.Write();
+  #if defined(DEBUG_ENABLED)
+    Serial.println(
+    "Ch1: "+String(data.ch[0])+" ,\
+    Ch2: "+String(data.ch[1])+" ,\
+    Ch3: "+String(data.ch[2])+" ,\
+    Ch4: "+String(data.ch[3])+" ,\
+    Ch5: "+String(data.ch[4])+" ,\
+    Ch6: "+String(data.ch[5])+" ,\
+    Ch7: "+String(data.ch[6])+" ,\
+    Ch8: "+String(data.ch[7])
+    );
+  #endif
+}
+
+//based on readAxis by johnwasser:https://forum.arduino.cc/t/adding-a-dead-zone-to-a-analog-imput-on-arduino-leonardo/662508/5
+int32_t readAxis(int8_t Axis_Packet, int AXIS_LOW, int AXIS_HIGH) {
+  const int DEADZONE_LOW  = 0-DEADZONE;
+  const int DEADZONE_HIGH = 0+DEADZONE;
+  const int AXIS_MID = ( (AXIS_LOW/2) + (AXIS_HIGH/2) );
+  if (Axis_Packet < DEADZONE_LOW) {
+    return map(Axis_Packet ,-128, DEADZONE_LOW, AXIS_LOW, AXIS_MID);
+  }
+  else if (Axis_Packet > DEADZONE_HIGH){
+    return map(Axis_Packet, DEADZONE_HIGH, 127, AXIS_MID, AXIS_HIGH);
+  }
+  else {
+    return AXIS_MID;
+  }
+}
+
+void ConvertLoraPacketToSBUS(byte dataPacket[]) {
+
+  //Extract joystick axis values from Packet (-128 to 127)
+  int8_t LxPacket = dataPacket[0];
+  int8_t LyPacket = dataPacket[1];
+  int8_t RxPacket = dataPacket[2];
+  int8_t RyPacket = dataPacket[3];
+
+  //Map Joystick Axis to SBUS Values
+  //Explanation: readAxis(Axis_Packet, LOWEST_PWM_VALUE_USED, HIGHEST_PWM_VALUE_USED)
+  int32_t Lx = readAxis(LxPacket, 432, 1552);
+  int32_t Ly = readAxis(-(LyPacket), 192, 1792); //This value is inverted on Unitree Go2, this makes joystick not inverted
+  int32_t Rx = readAxis(RxPacket, 432, 1552);
+  int32_t Ry = readAxis(RyPacket, 432, 1552);
+  //No deadzone filtering with this method
+  //int32_t Lx = map(LxPacket, -128, 127, 432, 1552); //Low: -128 Axis Value = 432 PWM, High: 127 Axis Value = 1552 PWM
+  //int32_t Ly = map(LyPacket, 127, -128, 192, 1792); //These are the expected values required for this particular application(Unitree Go2 Robot)
+  //int32_t Rx = map(RxPacket, -128, 127, 432, 1552); //For other RC applications these values will likely be different
+  //int32_t Ry = map(RyPacket, -128, 127, 432, 1552);
+
+  //Extract Button States from Packet
+  uint8_t Buttons_Byte1 = dataPacket[4];
+  uint8_t Buttons_Byte2 = dataPacket[5];
+
+  //Extract DPAD Button States from Buttons_Byte1
+  bool DPAD_UP, DPAD_DOWN, DPAD_RIGHT, DPAD_LEFT;
+  DPAD_UP = (Buttons_Byte1 & 0b00010000) != 0;
+  DPAD_DOWN = (Buttons_Byte1 & 0b00100000) != 0;
+  DPAD_RIGHT = (Buttons_Byte1 & 0b01000000) != 0;
+  DPAD_LEFT = (Buttons_Byte1 & 0b10000000) != 0;
+
+  //Extract Misc Button States from Buttons_Byte1
+  bool XBOX, START, SELECT, SHARE;
+  XBOX = (Buttons_Byte1 & 0b00000001) != 0;
+  SELECT = (Buttons_Byte1 & 0b00000010) != 0;
+  START = (Buttons_Byte1 & 0b00000100) != 0;
+  SHARE = (Buttons_Byte1 & 0b00001000) != 0;
+
+  //Extract All Button States from Buttons_Byte2
+  bool A, B, X, Y, L1, L2, R1, R2;
+  A = (Buttons_Byte2 & 0b00000001) != 0;
+  B = (Buttons_Byte2 & 0b00000010) != 0;
+  X = (Buttons_Byte2 & 0b00000100) != 0;
+  Y = (Buttons_Byte2 & 0b00001000) != 0;
+  L1 = (Buttons_Byte2 & 0b00010000) != 0;
+  R1 = (Buttons_Byte2 & 0b00100000) != 0;
+  L2 = (Buttons_Byte2 & 0b01000000) != 0;
+  R2 = (Buttons_Byte2 & 0b10000000) != 0;
+
+  //Show axis values and button states via the serial console
+  #if defined(DEBUG_ENABLED)
+    Serial.println(
+      "Lx: "+String(Lx)+" ,\
+      Ly: "+String(Ly)+" ,\
+      Rx: "+String(Rx)+" ,\
+      Ry: "+String(Ry)+" ,\
+      DPAD_UP: "+String(DPAD_UP)+" ,\
+      DPAD_DOWN: "+String(DPAD_DOWN)+" ,\
+      DPAD_RIGHT: "+String(DPAD_RIGHT)+" ,\
+      DPAD_LEFT: "+String(DPAD_LEFT)+" ,\
+      XBOX: "+String(XBOX)+" ,\
+      START: "+String(START)+" ,\
+      SELECT: "+String(SELECT)+" ,\
+      SHARE: "+String(SHARE)+" ,\
+      A: "+String(A)+" ,\
+      B: "+String(B)+" ,\
+      X: "+String(X)+" ,\
+      Y: "+String(Y)+" ,\
+      L1: "+String(L1)+" ,\
+      R1: "+String(R1)+" ,\
+      L2: "+String(L2)+" ,\
+      R2: "+String(R2)
+    );
+  #endif
+
+
+  //Initialize all channels with PWM values specific to the Unitree Go2 Application
+
+  // MODE FLAGS:
+  int32_t MODE1 = 992;
+  int32_t MODE2 = 192;
+  int32_t MODE3 = 1792;
+
+  int32_t _LOW = 192;
+  int32_t _MID = 992;
+  int32_t _HIGH = 1792;
+
+  int32_t CH1_STATE = Rx;
+  int32_t CH2_STATE = Ry;
+  int32_t CH3_STATE = Ly;
+  int32_t CH4_STATE = Lx;
+
+  int32_t CH5_STATE = MODE1;
+  int32_t CH6_STATE = _LOW;
+  int32_t CH7_STATE = _LOW;
+  int32_t CH8_STATE = _MID;
+
+  // Store Values Here to write to SBUS TX
+  SBUS_DATA tx_data;
+
+  tx_data.CH1 = CH1_STATE;
+  tx_data.CH2 = CH2_STATE;
+  tx_data.CH3 = CH3_STATE;
+  tx_data.CH4 = CH4_STATE;
+  tx_data.CH5 = CH5_STATE;
+  tx_data.CH6 = CH6_STATE;
+  tx_data.CH7 = CH7_STATE;
+  tx_data.CH8 = CH8_STATE;
+  
+  //The below code is various examples of mapping different Unitree Go2 8 channel SBUS Commands to various buttons on a XBOX Controller
+
+  // START BUTTON - Unlock
+  if(START == true){
+    if(START_Flag == 0){
+      CH5_STATE = MODE1;
+      CH7_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE1;
+      CH7_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE1;
+      CH7_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      START_Flag = 1;
+
+      return;      
+    }
+    else{}
+  }
+  else if(START_Flag = 1 && START == 0){
+    START_Flag = 0;
+  }
+
+  // SELECT BUTTON - Damping State
+  if(SELECT == 1){
+    if(SELECT_Flag == 0){
+      CH5_STATE = MODE1;
+      CH6_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE1;
+      CH6_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE1;
+      CH6_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      SELECT_Flag = 1;
+
+      return;      
+    }
+    else{}
+  }
+  else if(SELECT_Flag = 1 && SELECT == 0){
+    SELECT_Flag = 0;
+  }
+
+  // DPAD BUTTONS
+  // DPAD UP BUTTON - Fall Recover
+  if(DPAD_UP == 1){
+
+    if(D_UP_Flag == 0){
+      CH5_STATE = MODE2;
+      CH7_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE2;
+      CH7_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE2;
+      CH7_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      D_UP_Flag = 1;
+
+      return;
+      }  
+  }
+  else if(DPAD_UP == 0 && D_UP_Flag == 1){
+    D_UP_Flag = 0;
+  }
+  else{}
+
+  // DPAD DOWN BUTTON - STAND Toggle
+  if (DPAD_DOWN == true) {
+    if(D_DOWN_Flag == 0){
+
+      if(HIGH_STAND_Flag == 0){
+
+        CH5_STATE = MODE1;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE1;
+        CH8_STATE = _HIGH;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE1;
+        CH8_STATE = _LOW;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        D_DOWN_Flag = 1;
+        HIGH_STAND_Flag = 1;
+
+        return;
+
+      }
+      else if (HIGH_STAND_Flag == 1) {
+
+        CH5_STATE = MODE1;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE1;
+        CH8_STATE = _LOW;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE1;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        D_DOWN_Flag = 1;
+        HIGH_STAND_Flag = 0;
+
+        return;
+
+      }
+            
+    } 
+  }
+  else if(DPAD_DOWN == 0 && D_DOWN_Flag == 1){
+    D_DOWN_Flag = 0;
+  }
+  else{}
+
+  // DPAD RIGHT BUTTON - Light Toggle
+  if (DPAD_RIGHT == true) {
+    if(D_RIGHT_Flag == 0){
+
+      CH5_STATE = MODE3;
+      CH8_STATE = _MID;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH8 = CH8_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH8_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH8 = CH8_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH8_STATE = _MID;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH8 = CH8_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      D_RIGHT_Flag= 1;
+
+      return;
+    } 
+  }
+  else if(DPAD_RIGHT == 0 && D_RIGHT_Flag == 1){
+    D_RIGHT_Flag = 0;
+  }
+  else{}
+
+  // DPAD LEFT BUTTON - Continous Movement
+  if (DPAD_LEFT == 1) {
+    if(D_LEFT_Flag == 0){
+
+      CH5_STATE = MODE2;
+      CH6_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE2;
+      CH6_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE2;
+      CH6_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      D_LEFT_Flag= 1;
+
+      return;
+    } 
+  }
+  else if(DPAD_LEFT == 0 && D_LEFT_Flag == 1){
+    D_LEFT_Flag = 0;
+  }
+  else{}
+
+  // SHOULDER Buttons:
+  // R1 Button - Walking Gait Toggle 
+  if (R1 == true) {
+    if(R1_Flag == 0){
+
+      CH5_STATE = MODE3;
+      CH8_STATE = _MID;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH8 = CH8_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH8_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH8 = CH8_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH8_STATE = _MID;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH8 = CH8_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      R1_Flag= 1;
+
+      return;
+    } 
+  }
+  else if(R1 == 0 && R1_Flag == 1){
+    R1_Flag = 0;
+  }
+  else{}
+
+
+  // FACE Buttons:
+  // X BUTTON - Obstacle Avoidance Toggle
+  if (X == 1) {
+    if(X_Flag == 0){
+
+      if(OBSTACLE_Flag == 0){
+
+        CH5_STATE = MODE2;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE2;
+        CH8_STATE = _LOW;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE2;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        X_Flag = 1;
+        OBSTACLE_Flag = 1;
+
+        return;
+      }
+      else if (OBSTACLE_Flag == 1) {
+
+        CH5_STATE = MODE2;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE2;
+        CH8_STATE = _HIGH;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        CH5_STATE = MODE2;
+        CH8_STATE = _MID;
+        tx_data.CH5 = CH5_STATE;
+        tx_data.CH8 = CH8_STATE;
+
+        writeSBUS(tx_data);
+        delayMicroseconds(600);
+
+        X_Flag = 1;
+        OBSTACLE_Flag = 0;
+
+        return;
+      }
+    } 
+  }
+  else if(X_Flag == 1 && X == 0){
+    X_Flag = 0;
+  }
+  else{}
+
+  // Y BUTTON - Dance
+  if(Y == 1){
+    if(Y_Flag == 0){
+      CH5_STATE = MODE3;
+      CH7_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH7_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH7_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH7 = CH7_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      Y_Flag = 1;
+
+      return;      
+    }
+    else{}
+  }
+  else if(Y_Flag = 1 && Y == 0){
+    Y_Flag = 0;
+  }
+
+  // A BUTTON - Jump
+  if (A == 1) {
+    if(A_Flag == 0){
+
+      CH5_STATE = MODE3;
+      CH6_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH6_STATE = _HIGH;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      CH5_STATE = MODE3;
+      CH6_STATE = _LOW;
+      tx_data.CH5 = CH5_STATE;
+      tx_data.CH6 = CH6_STATE;
+
+      writeSBUS(tx_data);
+      delayMicroseconds(600);
+
+      A_Flag= 1;
+
+      return;
+    } 
+  }
+  else if(A == 0 && A_Flag == 1){
+    A_Flag = 0;
+  }
+  else{}
+
+  data.ch[0] = CH1_STATE;
+  data.ch[1] = CH2_STATE;
+  data.ch[2] = CH3_STATE;
+  data.ch[3] = CH4_STATE;
+  data.ch[4] = CH5_STATE;
+  data.ch[5] = CH6_STATE;
+  data.ch[6] = CH7_STATE;
+  data.ch[7] = CH8_STATE;
+  data.ch[8] = 1690; //CH9 static PWM value as shown in Unitree Go2 SBUS Documentation
+  data.ch[9] = 154; //CH10 static PWM value as shown in Unitree Go2 SBUS Documentation
+
+  //Output SBUS Data
+  sbus_tx.data(data);
+  sbus_tx.Write();
+  #if defined(DEBUG_ENABLED)
+    Serial.println(
+      "Ch1: "+String(data.ch[0])+" ,\
+      Ch2: "+String(data.ch[1])+" ,\
+      Ch3: "+String(data.ch[2])+" ,\
+      Ch4: "+String(data.ch[3])+" ,\
+      Ch5: "+String(data.ch[4])+" ,\
+      Ch6: "+String(data.ch[5])+" ,\
+      Ch7: "+String(data.ch[6])+" ,\
+      Ch8: "+String(data.ch[7])
+    );
+  #endif
+}
---
+++ RX.ino
@@ -0,0 +1,235 @@
+//WARNING!
+//The below code is a proof of concept only!
+//
+//DO NOT flash this code onto a device which is operated in a region with duty cycle restrictions/dwell time restrictions/air time restrictions
+//
+//The below code is intended to be ported to either FHSS with the ability to become more compliant with air time related restrictions
+//or ported to a device which operates on a frequency that does not have the same type of restrictions. 2.4ghz Lora?
+//
+//As it currently stands this code will not comply with duty cycle restrictions/dwell time restrictions/air time restrictions and needs further work before actual use
+//The author(s) take no responsibility for your use or misuse of this code.
+//It is up to the end user to research, know, and comply with all of the laws and regulations in the region in which they are operating in.
+//The end user, and only the end user, is responsible for the end user's actions.
+//
+//Again this is a proof of concept example only and not meant for actual use.  You have been warned!
+//
+//XBOX BT to SBUS related Code is forked from: https://github.com/mechzrobotics/Unitree_GO2_SBUS
+//LORA RX code based on example from: https://github.com/jgromes/RadioLib/blob/master/examples/SX126x/SX126x_Receive_Interrupt/SX126x_Receive_Interrupt.ino
+//LORA TX code based on example from: https://github.com/jgromes/RadioLib/blob/master/examples/SX126x/SX126x_Transmit_Interrupt/SX126x_Transmit_Interrupt.ino
+//Encoding/Decoding of LORA Packets containing XBOX Axis/Button States by Corey Harding
+//
+//Compatible Boards:
+//2.4ghz
+//Radiomaster_Ranger_Nano_ELRS_RCTX
+//Radiomaster_ER6_ELRS_RCRX
+//Lilygo_T3_S3_SX1280_WITH_PA
+//Lilygo_T3_S3_SX1280_WITHOUT_PA
+//915mhz
+//SeedStudio Xiao ESP32S3 plus WIO SX1262
+//Heltec WiFi LoRa 32 v3 (sx1262)
+//
+//Required 3rd party board manager urls:
+//Official ESP32 package: https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+//"Bluepad32 + ESP32" package: https://raw.githubusercontent.com/ricardoquesada/esp32-arduino-lib-builder/master/bluepad32_files/package_esp32_bluepad32_index.json
+//
+//Required libraries:
+//https://github.com/ESP32Async/AsyncTCP (Currently: 3.4.1)
+//https://github.com/ESP32Async/ESPAsyncWebServer (Currently: 3.7.7)
+//https://github.com/ayushsharma82/ElegantOTA (Currently: 3.1.7)
+//https://github.com/jgromes/RadioLib (Currently: 7.1.2)
+//https://github.com/bolderflight/sbus (Currently: 8.1.4)
+
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ElegantOTA.h>
+#include <RadioLib.h>
+#include "sbus.h"
+#include "settings.h" //Modify settings.h file to suite your hardware
+#include "ConvertLoraPacketToSBUS.h"
+
+//PacketRate-loop counter
+long lastMillis = 0;
+long PacketRatehz = 0;
+
+// flag to indicate that a packet was received
+volatile bool receivedFlag = false;
+
+// this function is called when a complete packet
+// is received by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+#if defined(ESP8266) || defined(ESP32)
+  ICACHE_RAM_ATTR
+#endif
+void setFlag(void) {
+  // we got a packet, set the flag
+  receivedFlag = true;
+}
+
+void setup() {
+  #if defined(DEBUG_ENABLED)
+    Serial.begin(115200);
+  #endif
+
+  //OTAUPDATE
+  WiFi.softAP(ssid, password);
+  #if defined(DEBUG_ENABLED)
+    Serial.println("");
+    Serial.println("SSID: ");
+    Serial.println(ssid);
+    Serial.println("Password: ");
+    Serial.println(password);
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  #endif
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
+
+  server.begin();
+  #if defined(DEBUG_ENABLED)
+    Serial.println("HTTP server started");
+  #endif
+
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  //ENDOTAUPDATE
+
+  //Turn on fans if available
+  #if defined(FAN_ENABLE)
+    pinMode(FAN_ENABLE, OUTPUT);
+    digitalWrite(FAN_ENABLE, HIGH);
+  #endif
+
+  //Antenna Control
+  #if defined(ANT_CTRL)
+    pinMode(ANT_CTRL, OUTPUT);
+    digitalWrite(ANT_CTRL, ANT_CTRL_HILO);
+  #endif
+
+  //Set Custom SPI Pins
+  #if defined(CUSTOM_SPI_PINS)
+    spi.begin(CUSTOM_SCK, CUSTOM_MISO, CUSTOM_MOSI, CUSTOM_CS);
+  #endif
+
+  // initialize radio with default settings
+  #if defined(DEBUG_ENABLED)
+    Serial.println(F("Initializing ... "));
+  #endif
+  int state = radio.begin(FREQ, BANDWIDTH, SPREADING_FACTOR, CODING_RATE_4X, SYNC_WORD, OUTPUT_POWER_DBM, PREAMBLE_LENGTH);
+  if (state == RADIOLIB_ERR_NONE) {
+    #if defined(DEBUG_ENABLED)
+      Serial.println(F("success!"));
+    #endif
+  } else {
+    #if defined(DEBUG_ENABLED)
+      Serial.println(F("failed, code "));
+      Serial.println(state);
+    #endif
+    while (true) { delay(10); }
+  }
+
+  //Enable RF switch if required by module
+  #if defined(RADIO_RX_PIN) || defined(RADIO_TX_PIN)
+      //Set RX/TX Enable Pins
+      radio.setRfSwitchPins(RADIO_RX_PIN, RADIO_TX_PIN);
+  #endif
+
+  radio.implicitHeader(PACKET_LENGTH);
+  radio.setCRC(0);
+
+  //Setup manual gain
+  #if defined(MANUAL_GAIN)
+      //Set RX/TX Enable Pins
+      radio.setGainControl(MANUAL_GAIN);
+  #endif
+
+  // set the function that will be called
+  // when new packet is received
+  radio.setPacketReceivedAction(setFlag);
+
+  // start listening for LoRa packets
+  #if defined(DEBUG_ENABLED)
+    Serial.println(F("Starting to listen ... "));
+  #endif
+  state = radio.startReceive();
+  if (state == RADIOLIB_ERR_NONE) {
+    #if defined(DEBUG_ENABLED)
+      Serial.println(F("success!"));
+    #endif
+  } else {
+    #if defined(DEBUG_ENABLED)
+      Serial.println(F("failed, code "));
+      Serial.println(state);
+    #endif
+    while (true) { delay(10); }
+  }
+
+  /* Begin the SBUS communication */
+  sbus_tx.Begin();
+
+}
+
+void loop() {
+  //PacketRate in "hz" timer
+  long currentMillis = millis();
+
+  //OTAUPDATE
+  ElegantOTA.loop();
+  //ENDOTAUPDATE
+
+  // check if the flag is set
+  if(receivedFlag) {
+    // reset flag
+    receivedFlag = false;
+
+    // you can read received data as an Arduino String
+    //String dataPacketStr;
+    //int state = radio.readData(dataPacketStr);
+
+    byte dataPacket[PACKET_LENGTH];
+    int state = radio.readData(dataPacket, PACKET_LENGTH);
+
+    if (state == RADIOLIB_ERR_NONE) {
+      // print data of the packet
+      #if defined(DEBUG_ENABLED)
+        Serial.println(F("Data:\t"));
+      //Serial.println(dataPacketStr);
+      #endif
+
+      //Send controller data to SBUS Conversion function to be Converted to PWM values and TX via SBUS
+      
+      ConvertLoraPacketToSBUS(dataPacket);
+
+      PacketRatehz++; //increase PacketRate variable
+    } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
+      // packet was received, but is malformed
+      #if defined(DEBUG_ENABLED)
+        Serial.println(F("CRC error!"));
+      #endif
+
+    } else {
+      // some other error occurred
+      #if defined(DEBUG_ENABLED)
+        Serial.println(F("failed, code "));
+        Serial.println(state);
+      #endif
+
+    }
+    #if defined(USING_SX1280) || defined(USING_SX1281)
+      // put module back to listen mode
+      radio.startReceive();
+    #endif
+  }
+      //PacketRate-hz
+  if(currentMillis - lastMillis > 1000){
+    #if defined(DEBUG_ENABLED)
+      Serial.println("PacketRate in hz: "+String(PacketRatehz));
+    #endif
+    lastMillis = currentMillis;
+    PacketRatehz = 0;
+  }
+  vTaskDelay(1);
+  delayMicroseconds(600);
+}
